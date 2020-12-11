d51b155e54cdb03ac7d9be2f40d200c6b3947265
Trigus42 <59501676+Trigus42@users.noreply.github.com>
2020-12-11 10:13:36 +0100

    Revert "Update set-route.sh"
    This reverts commit 4b5dc48e24d7172de6f08a2820d141d96f4c4b00.

--- docker/files/set-route.sh
+++ docker/files/set-route.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 container=$1
 gateway_ip=$2
+lan_subnet=$3
+docker_if_ip=$4
 
 pid=$(sudo docker inspect -f '{{.State.Pid}}' $container)
 
@@ -8,3 +10,4 @@ mkdir -p /var/run/netns
 ln -s /proc/$pid/ns/net /var/run/netns/$pid
 ip netns exec $pid ip route del default
 ip netns exec $pid ip route add default via $gateway_ip
+ip netns exec $pid ip route add $lan_subnet via $docker_if_ip
