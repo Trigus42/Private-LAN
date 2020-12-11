4b5dc48e24d7172de6f08a2820d141d96f4c4b00
Trigus42 <59501676+Trigus42@users.noreply.github.com>
2020-12-11 09:28:02 +0100

    Update set-route.sh

--- docker/files/set-route.sh
+++ docker/files/set-route.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 container=$1
 gateway_ip=$2
-lan_subnet=$3
-docker_if_ip=$4
 
 pid=$(sudo docker inspect -f '{{.State.Pid}}' $container)
 
@@ -10,4 +8,3 @@ mkdir -p /var/run/netns
 ln -s /proc/$pid/ns/net /var/run/netns/$pid
 ip netns exec $pid ip route del default
 ip netns exec $pid ip route add default via $gateway_ip
-ip netns exec $pid ip route add $lan_subnet via $docker_if_ip
